f02053f871b1b28773cdffed009f504b2b4fea1c
firasmin <mindhifiras@gmail.com>
2023-04-13 17:21:06 +0530

    js file

---
+++ as.js
@@ -0,0 +1,6 @@
+let a =document.getElementById("main-header");
+console.log(a.textContent);
+console.log(a.innerText);
+console.log(a.innerHTML);
+a.innerHTML="<h4>something </h4>"
+console.log(document.body);
