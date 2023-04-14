a04fe3c1707b61ca136fa5b74910a9c23e3386b6
firasmin <mindhifiras@gmail.com>
2023-04-15 01:45:34 +0530

    qasw

--- aq.js
+++ aq.js
@@ -25,7 +25,39 @@
 // {
 //     x[i].style.backgroundColor="#f4f4f4";
 // }
-document.querySelector(".list-group-item:nth-child(2)").style.backgroundColor="green";
 
-document.querySelector(".list-group-item:nth-child(3)").style.visibility  = 'hidden';
 
+// document.querySelector(".list-group-item:nth-child(2)").style.backgroundColor="green";
+
+// document.querySelector(".list-group-item:nth-child(3)").style.visibility  = 'hidden';
+
+// let x= document.querySelectorAll(".list-group-item:nth-child(odd)");
+// let y= document.querySelectorAll(".list-group-item:nth-child(even)");
+
+
+// for(let i=0;i<x.length;i++)
+// {
+//     x[i].style.backgroundColor="#ccc";
+//     y[i].style.backgroundColor="#f4f4f4";
+
+// }
+//var itemlist=document.querySelector("#items");
+//console.log(itemlist.parentElement);
+var newdiv =document.createElement("div");
+newdiv.className="hello";
+newdiv.id="hello1";
+let divtext =document.createTextNode("hello");
+newdiv.appendChild(divtext);
+let container=document.querySelector('header .container');
+let h1=document.querySelector('header h1');
+
+container.insertBefore(newdiv,h1);
+newdiv.style.fontSize="50px";
+let li =document.createElement('div');
+let text=document.createTextNode('water');
+li.appendChild(text);
+let y=document.getElementById('items')
+let as=y.insertBefore(li,y.children[0]);
+console.log(li);
+li.style.backgroundColor="#ccc"
+li.style.fontSize="30px";
