f9f03ad85e9612ab0a7a62f38d42cd3bbf4cd0cd
firasmin <mindhifiras@gmail.com>
2023-06-29 17:48:00 +0530

    different req url

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-const http= require('http')
+const http= require('http');
 const server =http.createServer((req,res)=>
 {
-    console.log("hello world")
+    console.log(req.url,req.method,req.headers)
+    if(req.url=='/connect')
+    {
+    res.setHeader('Content-Type','text/html')
+    res.write('<html><head><title>my first page </title><body><h1> this is node js </h1></body> </head></html>')
+    res.end()
+    }
+    else if(req.url=='/home')
+    {
+    res.setHeader('Content-Type','text/html')
+    res.write('<html><head><title>my first page </title><body><h1> welcome home </h1></body> </head></html>')
+    res.end()
+    }
+    else if(req.url=='/about')
+    {
+    res.setHeader('Content-Type','text/html')
+    res.write('<html><head><title>my first page </title><body><h1> welcome to abou us</h1></body> </head></html>')
+    res.end()
+    }
+    else{
+     res.setHeader('Content-Type','text/html')
+    res.write('<html><head><title>my first page </title><body><h1> nothing</h1></body> </head></html>')
+    res.end()   
+    }
 })
 server.listen(4000)
