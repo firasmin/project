00045112ac8b024b842b0c1da862febe26b850d7
firasmin <mindhifiras@gmail.com>
2023-07-03 20:41:53 +0530

    router and filter

--- app.js
+++ app.js
@@ -2,20 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+const adminrouter=require('./routes/admin')
+const shoproute=require('./routes/shop')
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="product_name"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
-
+app.use('/admin',adminrouter);
+app.use(shoproute);
+app.use((req,res,next)=>
+{
+    res.status(404).send("<h1>page not found</h1>")
+})
 app.listen(3000);
