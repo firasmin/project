4f2a11f5b7fe1ce910507b407d6d3748db8272c3
firasmin <mindhifiras@gmail.com>
2023-07-03 16:19:24 +0530

    express

--- routing_req.js
+++ routing_req.js
@@ -1,6 +1,15 @@
-const http = require('http');
-const route =require('./routes')
-console.log(route.some)
-const server = http.createServer(route.handeler);
+const express=require('express');
+const app=express()
+app.use((req,res,next)=>
+{
+    console.log("this is middleware")
+     next()
+})
+app.use((req,res,next)=>
+{
+    console.log("this is  next middleware")
+    res.send("<h1>hello from express js</h1>")
+})
 
-server.listen(3000);
+
+app.listen(3000);
