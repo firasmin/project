f4bb17a0cafe6f46b037747b43e4a56fc8df19c7
firasmin <mindhifiras@gmail.com>
2023-07-06 21:57:11 +0530

    model2

--- models/product.js
+++ models/product.js
@@ -1,42 +1,47 @@
-const { json } = require('body-parser')
-const fs =require('fs')
-const path=require('path')
+const fs = require('fs');
+const path = require('path');
 
-const Paths=path.join(path.dirname(process.mainModule.filename),'data','products.json')
-const getproductfile=cb=>{
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-    fs.readFile(Paths,(err,filecontent)=>
-    {
-        if(err)
-        {
-            cb([])
-        }
-        else{
-        cb(JSON.parse(filecontent))
-        }
-    })
-}
-   
-module.exports=class Product
-{
-   constructor(title)
-   {
-    this.title=title
-   }
-   save()
-   {
-      getproductfile(products=>{
-        products.push(this)
-        fs.writeFile(Paths,JSON.stringify(products),(err)=>
-        {
-            console.log(err)
-        })
-      })
-        
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
-   }
-   static fetchall(cb)
-   {
-    getproductfile(cb)
-   }
-}
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id=Math.random().toString()
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
+};
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,20 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const productcontroller=require('../controller/production')
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-router.get('/',productcontroller.getproduct);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
