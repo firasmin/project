f7254b62bf1dcc7c2cd0d7e3df09f31db8882de9
firasmin <mindhifiras@gmail.com>
2023-07-29 16:08:21 +0530

    secure login

--- expess app/router.js
+++ expess app/router.js
@@ -1,9 +1,10 @@
 const express=require('express')
 const router=express.Router()
-
+const bcrypt=require('bcrypt')
 const path=require('path')
 const model=require('./model')
 const root_dir=require('../util/path')
+const { Error } = require('sequelize')
 function IsStringInvalid(str)
 {
     if(str==undefined||str.length===0)
@@ -27,12 +28,16 @@ router.post('/expense',async(req,res,next)=>{
     {
         return res.status(400).json({err:"something is missing"})
     }
-   await model.create({
-        name:name,
-        email:email,
-        password:password
+    bcrypt.hash(password,10,async(err,hash)=>{
+        console.log(err)
+ await model.create({
+        name,
+        email,
+        password:hash
     })
     res.status(201).json({message:"successs"})
+    })
+  
     
 }catch(err){
 res.status(500).json({error:err})
@@ -44,22 +49,41 @@ router.get('/login',(req,res,next)=>{
     res.sendFile(path.join(root_dir,'login.html'))
 })
 router.post('/login',async(req,res)=>{
+    try{
+
     email=req.body.email
     password=req.body.password
     console.log(email)
-    const a=await model.findOne({where:{email:`${email}`}})
-    if(a==null)
+    const a=await model.findAll({where:{email:`${email}`}})
+    if(IsStringInvalid(email)||IsStringInvalid(password))
     {
-        res.status(404).json({error:'email is wrong'})
+        return res.status(400).json({success:false,err:"something is missing"})
+    }
+    if(a.length>0)
+    {
+        bcrypt.compare(password,a[0].password,(err,result)=>{
+          if(err)
+          {
+            throw new Error('SOMETHING WENT WRONG')
+          } 
+          if(result==true)
+          {
+
+            res.status(200).json({success:true,message:"login successfull"})
+        }
+        else{
+         res.status(402).json({success:false,error:"password is wrong"})
+        }
+        })
+       
     }
     else{
-       if(password==a.password)
-       {
-        res.status(200).json({message:"login successfull"})
-       }
-       else{
-        res.status(402).json({error:"password is wrong"})
-       }
+        
+        res.status(404).json({success:false,error:'email is wrong'})
+       
     }
+}catch(err){
+    res.status(500).json({error:err})
+}
 })
 module.exports=router
